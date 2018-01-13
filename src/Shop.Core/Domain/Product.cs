af64a839fae0c56eb7b5657a9ab729ddb4446eab
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-13 12:16:56 +0100

    Added product, removed views

---
+++ src/Shop.Core/Domain/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Core.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Category { get; }
+        public decimal Price { get; }
+
+        public Product(string name, string category, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+    }
+}
