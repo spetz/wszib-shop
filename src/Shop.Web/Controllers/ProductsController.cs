137ee87cbcaf33db9bc06bb78385b344c0e237b5
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-13 12:53:15 +0100

    Products list

---
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+using System.Collections.Generic;
+
+namespace Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static readonly List<Product> _products = new List<Product>
+        {
+            new Product("Laptop", "Electronics", 3000),
+            new Product("Jeans", "Trousers", 150),
+            new Product("Hammer", "Tools", 47)
+        };
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+    }
+}
