ce5a64a7a2f96b0ea7041ff61da07f394a9da0ac
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-13 14:54:25 +0100

    Add product

--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
+using Shop.Web.Models;
 using System.Collections.Generic;
 
 namespace Shop.Web.Controllers
 {
+    [Route("products")]
     public class ProductsController : Controller
     {
         private static readonly List<Product> _products = new List<Product>
@@ -13,9 +15,30 @@ namespace Shop.Web.Controllers
             new Product("Hammer", "Tools", 47)
         };
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_products);
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            var viewModel = new ProductViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(ProductViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            _products.Add(new Product(viewModel.Name, viewModel.Category, viewModel.Price));
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ src/Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Models
+{
+    public class ProductViewModel
+    {
+        [Required(ErrorMessage = "Name was not provided.")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        public List<SelectListItem> Categories { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Text = "Electronics", Value = "Electronics"},
+            new SelectListItem { Text = "Tools", Value = "Tools"},
+            new SelectListItem { Text = "Trousers", Value = "Trousers"}
+        };
+    }
+}
