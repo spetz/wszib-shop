4db5f4107139bc97c2e9c8e420b6b426d59d1c07
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-27 16:29:40 +0100

    Remove from cart

--- src/Shop.Web/wwwroot/js/site.js
+++ src/Shop.Web/wwwroot/js/site.js
@@ -6,6 +6,22 @@
             var productId = $(this).data('id');
             addCartItem(productId);
         })
+
+        $('.remove-from-cart').click(function () {
+            var item = $(this);
+            var productId = item.data('id');
+            var quantity = parseInt(item.data('quantity'));
+            removeCartItem(productId).then(function (response) {
+                if (quantity === 1) {
+                    item.parent().parent().fadeOut();
+
+                    return;
+                }
+                var updatedQuantity = --quantity;
+                item.data('quantity', updatedQuantity);
+                $(`#quantity-${productId}`).text(updatedQuantity);
+            });
+        })
     }
     init();
 
@@ -13,5 +29,13 @@
         $.post(`cart/items/${productId}`, response => {
             console.log(`Product with id: ${productId} was added to the cart.`);
         }); 
+    }
+
+    //Return 'promise'
+    function removeCartItem(productId) {
+        return $.ajax({
+            type: 'DELETE',
+            url: `cart/items/${productId}`
+        });
     }
 })();
