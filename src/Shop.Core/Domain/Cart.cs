8bc82ce1bffdfb2edfd9d9aead58f4fbe77f6d9b
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-27 09:26:08 +0100

    Cart & Cart item

---
+++ src/Shop.Core/Domain/Cart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Core.Domain
+{
+    public class Cart
+    {
+        private ISet<CartItem> _items = new HashSet<CartItem>();
+        public IEnumerable<CartItem> Items => _items;
+        public decimal TotalPrice => Items.Sum(i => i.TotalPrice);
+
+        public void AddProduct(Product product)
+        {
+            var item = _items.SingleOrDefault(x => x.ProductId == product.Id);
+            if (item == null)
+            {
+                item = new CartItem(product);
+                _items.Add(item);
+
+                return;
+            }
+            item.IncreaseQuantity();
+        }
+
+        public void DeleteProduct(Guid productId)
+        {
+            var item = _items.SingleOrDefault(x => x.ProductId == productId);
+            if (item == null)
+            {
+                throw new Exception($"Product with id: '{productId}' was not found in cart.");
+            }
+            if (item.Quantity == 1)
+            {
+                _items.Remove(item);
+
+                return;
+            }
+            item.DecreaseQuantity();
+        }
+
+        public void Clear() => _items.Clear();
+    }
+}
---
+++ src/Shop.Core/Domain/CartItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Shop.Core.Domain
+{
+    public class CartItem
+    {
+        public Guid ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
+
+        public CartItem(Product product)
+        {
+            ProductId = product.Id;
+            ProductName = product.Name;
+            Quantity = 1;
+            UnitPrice = product.Price;
+        }
+        
+        public void IncreaseQuantity()
+        {
+            Quantity++;
+        }
+
+        public void DecreaseQuantity()
+        {
+            if (Quantity == 1)
+            {
+                throw new Exception("Quantity can not be lower than 1.");
+            }
+            Quantity--;
+        }
+    }
+}
