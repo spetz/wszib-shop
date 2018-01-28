ee31bf7d933c981047ce62ae020b2bffba660e6f
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-28 11:58:50 +0100

    Login test

--- tests/Shop.Tests/Services/UserServiceTests.cs
+++ tests/Shop.Tests/Services/UserServiceTests.cs
@@ -6,19 +6,28 @@ using Shop.Core.Domain;
 using Shop.Core.DTO;
 using Shop.Core.Repositories;
 using Shop.Core.Services;
+using System;
 using Xunit;
 
 namespace Shop.Tests.Services
 {
     public class UserServiceTests
     {
+        Mock<IUserRepository> userRepositoryMock;
+        Mock<IMapper> mapperMock;
+        Fixture fixture;
+
+        public UserServiceTests()
+        {
+            userRepositoryMock = new Mock<IUserRepository>();
+            mapperMock = new Mock<IMapper>();
+            fixture = new Fixture();
+        }
+
         [Fact]
         public void get_should_return_user()
         {
             //Arrange
-            var userRepositoryMock = new Mock<IUserRepository>();
-            var mapperMock = new Mock<IMapper>();
-            var fixture = new Fixture();
             var user = fixture.Create<User>();
             var userDto = fixture.Create<UserDto>();
             userRepositoryMock.Setup(x => x.Get(user.Email)).Returns(user);
@@ -33,6 +42,20 @@ namespace Shop.Tests.Services
             expectedUserDto.Should().NotBeNull();
             userRepositoryMock.Verify(x => x.Get(user.Email), Times.Once);
             mapperMock.Verify(x => x.Map<UserDto>(user), Times.Once);
+        }
+
+        [Fact]
+        public void login_should_fail_for_non_existing_user()
+        {
+            //Arrange
+            var user = fixture.Create<User>();
+            IUserService userService = new UserService(userRepositoryMock.Object,
+                mapperMock.Object);
+
+            //Act & Assert
+            Action login = () => userService.Login(user.Email, user.Password);
+            var exceptionAssertion = login.ShouldThrow<Exception>();
+            exceptionAssertion.And.Message.Should().BeEquivalentTo($"User '{user.Email}' was not found.");
         }
     }
 }
