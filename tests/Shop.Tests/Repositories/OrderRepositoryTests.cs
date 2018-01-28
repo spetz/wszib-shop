3ba908f6871c2fed9fef0871d2f52eaaf7c46282
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-28 09:23:22 +0100

    OrderRepository tests

---
+++ tests/Shop.Tests/Repositories/OrderRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Shop.Core.Domain;
+using Shop.Core.Repositories;
+using System.Linq;
+using Xunit;
+
+namespace Shop.Tests.Repositories
+{
+    public class OrderRepositoryTests
+    {
+        [Fact]
+        public void adding_an_order_should_succeed()
+        {
+            //Arrange
+            IOrderRepository orderRepository = new OrderRepository();
+            var cart = new Cart();
+            cart.AddProduct(new Product("test", "test", 1));
+            var order = new Order(new User("test", "test"), cart);
+
+            //Act
+            orderRepository.Add(order);
+
+            //Assert
+            var expectedOrder = orderRepository.Get(order.Id);
+            Assert.Equal(expectedOrder, order);
+
+            var orders = orderRepository.Browse(order.UserId);
+            Assert.NotEmpty(orders);
+            Assert.Single(orders);
+            Assert.Contains(orders, o => o.Id == order.Id);
+        }
+    }
+}
