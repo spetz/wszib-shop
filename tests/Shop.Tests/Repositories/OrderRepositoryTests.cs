46ce6ab7851065c98c2ef4a295313c915a55e8ce
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-28 09:46:04 +0100

    Order repository fluent test

--- tests/Shop.Tests/Repositories/OrderRepositoryTests.cs
+++ tests/Shop.Tests/Repositories/OrderRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using Shop.Core.Domain;
+﻿using AutoFixture;
+using FluentAssertions;
+using Shop.Core.Domain;
 using Shop.Core.Repositories;
 using System.Linq;
 using Xunit;
@@ -27,6 +29,31 @@ namespace Shop.Tests.Repositories
             Assert.NotEmpty(orders);
             Assert.Single(orders);
             Assert.Contains(orders, o => o.Id == order.Id);
+        }
+
+        [Fact]
+        public void adding_an_order_should_succeed_v2()
+        {
+            //Arrange
+            IOrderRepository orderRepository = new OrderRepository();
+            var fixture = new Fixture();
+            var cart = fixture.Create<Cart>();
+            var product = fixture.Create<Product>();
+            var user = fixture.Create<User>();
+            cart.AddProduct(product);
+            var order = new Order(user, cart);
+
+            //Act
+            orderRepository.Add(order);
+
+            //Assert
+            var expectedOrder = orderRepository.Get(order.Id);
+            expectedOrder.ShouldBeEquivalentTo(order);
+
+            var orders = orderRepository.Browse(order.UserId);
+            orders.Should().NotBeEmpty();
+            orders.Should().ContainSingle();
+            orders.Should().Contain(o => o.Id == order.Id);
         }
     }
 }
