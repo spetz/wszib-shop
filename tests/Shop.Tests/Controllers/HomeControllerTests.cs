d0c50a0cde085ad91f70399a0db1520f4a24b0dd
Piotr Gankiewicz <gankiewi@teacher.wszib.edu.pl>
2018-01-28 10:34:36 +0100

    User repository & home controller test

---
+++ tests/Shop.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Controllers;
+using Shop.Web.Models;
+using Xunit;
+
+namespace Shop.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void index_method_should_return_view()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void error_method_should_return_view_with_view_model()
+        {
+            var controller = new HomeController();
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            var result = controller.Error() as ViewResult;
+
+            result.Should().NotBeNull();
+            var viewModel = result.Model as ErrorViewModel;
+            viewModel.Should().NotBeNull();
+        }
+    }
+}
---
+++ tests/Shop.Tests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Shop.Core.Domain;
+using Shop.Core.Repositories;
+using Xunit;
+
+namespace Shop.Tests.Repositories
+{
+    public class UserRepositoryTests
+    {
+        [Fact]
+        public void adding_the_user_should_succeed()
+        {
+            //Arrange
+            IUserRepository userRepository = new UserRepository();
+            var fixture = new Fixture();
+            var user = fixture.Create<User>();
+
+            //Act
+            userRepository.Add(user);
+
+            //Assert
+            var expectedUserById = userRepository.Get(user.Id);
+            var expectedUserByEmail = userRepository.Get(user.Email);
+
+            expectedUserById.Should().NotBeNull();
+            expectedUserByEmail.Should().NotBeNull();
+            user.ShouldBeEquivalentTo(expectedUserById);
+            user.ShouldBeEquivalentTo(expectedUserByEmail);
+        }
+    }
+}
